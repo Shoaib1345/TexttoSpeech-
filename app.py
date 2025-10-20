0161cf57c26f2ad1e935fcff4f8445581a597c44
Shoaib1345 <shoaibahmedprogramming@gmail.com>
2025-10-20 11:55:24 +0500

    frontend

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def hello_world():
+    # Corrected 'render_tempate' → 'render_template'
+    return render_template("frontend/index.html")
 
 if __name__ == '__main__':
     app.run()
